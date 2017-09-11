b069b2aeabc8f2b51a5e9ed4e605077154f5f979
amelia <yu.a@live.com>
2017-09-11 13:34:06 +0200

    acheter + localisation2

---
+++ resources/views/pages/utilisateur/partials/nav_localisation.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default navi">
+<div class="col-xs-4">
+    <a id="acheter" href="{{url('/')}}">
+        <img id="precedent" class="img-responsive" src="{{url('/images/picto/precedent.png')}}"></a>
+    </div>
+
+         <div class="col-md-8 recherche">
+            <div id="custom-search-input">
+                <div class="input-group col-md-12">
+                    <input type="text" class="form-control input-lg" placeholder="Chercher un magasin" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ resources/views/pages/utilisateur/sacs_perso.blade.php
@@ -0,0 +1,87 @@
+@extends('template.pages_temp')
+@section('content')
+
+<div class="row produit">
+    <div class="container">
+            <div class="col-xs-4">
+                <img id="sac_toile" src="/images/photos_sacs/package.jpg"><br>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <label class="sr-only" for="exampleInputAmount">1</label>
+                        <div class="input-group">
+                        <div class="input-group-addon">x</div>
+                        <input type="text" class="form-control" id="exampleInputAmount" placeholder="1">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-8">
+                    <h3>Sac en toile</h3>
+                    <button type="button" class="btn btn-default btn-produit" data-toggle="modal" data-target="#myModal">
+                        <img src="/images/picto/description.png" class="img-responsive description">
+                        Description
+                    </button><br>
+
+                    
+                    <div class=" col-xs-12 modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="color:white">&times;</span>
+                                    </button>
+                                    <h4 class="modal-title" id="myModalLabel">Sac en toile</h4>
+                                </div>
+                                <div class="modal-body">
+                                <h4 class="sous_titre_modal">DESCRIPTION:</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra justo et tincidunt feugiat. Duis varius at massa eget mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                                </p>
+                                <h4 class="sous_titre_modal">DIMENSIONS:</h4>
+                                <p>Integer pharetra odio at ligula tempu.</p>
+                                <h4 class="sous_titre_modal">MATIÈRE:</h4>
+                                <p>nec ullamcorper mauris feugiat. </p>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-produit">
+                    <img src="/images/picto/ajouter_panier.png" class="img-responsive description">
+                        Mettre dans le panier
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="espace row btn-acheter">
+    <div class="container">
+        <div class="col-xs-12">
+            <a id="lien_btn" href="{{url('#')}}">
+                <div class="cercle">
+                    <img class="picto_btn img-responsive" src="/images/picto/sac_perso.png">
+                </div>
+                <p class="calltoaction">
+                Acheter des sacs
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="espace row btn-acheter">
+    <div class="container">
+        <div class="col-xs-12">
+            <a id="lien_btn" href="{{url('#')}}">
+                <div class="cercle">
+                    <img class="picto_btn img-responsive" src="/images/picto/panier_check.png">
+                </div>
+                <p class="calltoaction">
+                Acheter des sacs
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
+
+@endsection
